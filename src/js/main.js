29da1ee5f7e72773e2c6b2a459ac13d3d1b259e7
Dmitry <lvbnhbq2103@mail.ru>
2017-12-17 19:07:47 +0700

    Home

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+  $(".butter").click(function(){
+    $(".header__menu--container").toggleClass("header__menu--open");
+  });
+
+  $(".header__submenu_arrow").click(function(){
+    $(this).parents(".header__menu_item").toggleClass("header__submenu--open");
+    $(this).parents(".header__menu_item").find(".header__submenu").toggleClass("header__submenu--opened");
+  });
+
   
   new WOW().init();
 });
