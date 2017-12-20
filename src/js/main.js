885eb5ea0af8cba57abafd24feaf34997eed2a8d
Dmitry <lvbnhbq2103@mail.ru>
2017-12-20 13:28:13 +0700

    Front-2

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+
+  $(function() { 
+    $(window).scroll(function() {     
+      if($(this).scrollTop() >300) {     
+        $('#toTop').fadeIn();     
+      } else {     
+        $('#toTop').fadeOut();     
+      }     
+    });     
+    $('#toTop').click(function() {     
+      $('body,html').animate({scrollTop:0},800);     
+    });     
+  });
+
   $(".butter").click(function(){
     $(".header__menu--container").toggleClass("header__menu--open");
   });
@@ -8,8 +22,48 @@ $(document).ready(function() {
     $(this).parents(".header__menu_item").find(".header__submenu").toggleClass("header__submenu--opened");
   });
 
+  $("#slider").slick({
+    dots: true,
+    arrows: false
+  });
   
   new WOW().init();
+
+
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [56.288161, 43.916541],
+            zoom: 17
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/marker.png',
+            // Размеры метки.
+            iconImageSize: [75, 104],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-20, -105]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark)
+  });
+
+
 });
 
 
